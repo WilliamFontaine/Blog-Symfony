d6b42fcdec165f9985fe244b20034ce046c9701f
William Fontaine <william.fontaine0308@gmail.com>
2022-07-15 21:58:28 +0200

    📦 NEW: navbar

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -1,8 +1,19 @@
-function myFunction() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
+const toggler = document.querySelector('.hamburger');
+const navLinksContainer = document.querySelector('.navlinks-container');
+
+const toggleNav = () => {
+  toggler.classList.toggle('open');
+
+  navLinksContainer.classList.toggle('open');
 }
+
+toggler.addEventListener('click', toggleNav);
+
+new ResizeObserver(entries => {
+  if (entries[0].contentRect.width <=900) {
+    navLinksContainer.style.transition = "transform 0.3s ease-out";
+  }
+  else {
+    navLinksContainer.style.transition = "none";
+  }
+}).observe(document.body);
